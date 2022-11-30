5c19c869576dec1dcc17b7b361e8a05ae0fb5a9a
Fo-Zi <facundozingarelli@gmail.com>
2022-11-30 11:18:29 -0300

    Setting GH page

---
+++ docs/html/search/typedefs_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['node_0',['Node',['../LinkedList_8h.html#ac09cf950484bd9550d14d602b0e5e7fb',1,'LinkedList.h']]],
+  ['nodeptr_1',['NodePtr',['../LinkedList_8h.html#aa6eec21621b8638e350fd0ec238a9715',1,'LinkedList.h']]]
+];
---
+++ docs/html/search/variables_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['tail_0',['tail',['../structlist.html#aa6af5353c9a8a524c48539a57a8063f3',1,'list']]]
+];
